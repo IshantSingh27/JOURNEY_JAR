c6d2db24f4ad1cdcc38900989c0b8ea284567ad9
Mrinal Chaubey <mrinalchaubey7@gmail.com>
2025-12-10 14:57:07 +0530

    Your correct message

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,25 @@
-import React from "react"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Home from "./pages/Home/home"
-import Login from "./pages/Auth/login"
-import SignUp from "./pages/Auth/Signup"
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/home";
+import Login from "./pages/Auth/login";
+import SignUp from "./pages/Auth/Signup";
 
 const App = () => {
   return (
     <>
+      <h2 style={{ color: "green", textAlign: "center", marginTop: "20px" }}>
+        App is working!
+      </h2>
+
       <BrowserRouter>
         <Routes>
-          <Route element={<PrivateRoute />}>
-            <Route path="/" exact element={<Home />} />
-          </Route>
-
-          <Route path="/login" exact element={<Login />} />
-          <Route path="/sign-up" exact element={<SignUp />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
