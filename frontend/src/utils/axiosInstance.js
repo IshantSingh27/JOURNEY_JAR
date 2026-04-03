0803f0b7e86fe5aa64e2fb3facc8abacca39418b
IshantSingh27 <148959807+IshantSingh27@users.noreply.github.com>
2026-04-03 22:10:40 +0530

    Update axiosInstance.js

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "http://localhost:3000/api"
+const BASE_URL = "https://journey-jar-backend.onrender.com"
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
