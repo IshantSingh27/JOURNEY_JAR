41c3c1fdcd6c990dee6838a77cda6cdf1bef1f19
IshantSingh27 <ishantsingh0602@gmail.com>
2026-07-29 02:10:22 +0530

    fix: append /api to BASE_URL in axios config

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const BASE_URL = "https://journey-jar-backend.onrender.com"
+export const BASE_URL = "https://journey-jar-backend.onrender.com/api"
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
