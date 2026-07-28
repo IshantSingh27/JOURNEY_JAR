c4fc6d722c296faa2dfe8f64fc8c8bf12b9a733f
IshantSingh27 <ishantsingh0602@gmail.com>
2026-07-29 02:03:45 +0530

    fix: update backend URL in axios config

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "https://journeyjar-3.onrender.com/api"
+const BASE_URL = "https://journey-jar-backend.onrender.com"
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
